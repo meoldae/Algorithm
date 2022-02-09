41838c0e8cd1e4bd6848e8183ea6fe676d3918f9
Junyoung <wnsdud12365@naver.com>
2022-02-09 19:37:55 +0900

    20220209

---
+++ Boj/감소하는수.py
@@ -0,0 +1,23 @@
+from itertools import combinations
+
+nums = [i for i in range(10)]
+N = int(input()) 
+
+answer = []
+for i in range(1, 11):
+    temp = list(map(list, combinations(nums, i)))
+        
+    for j in range(len(temp)):
+        temp[j] = sorted(temp[j], reverse=True)
+    
+    for j in range(len(temp)):
+        answer.append(int(''.join(map(str, temp[j]))))
+    
+answer.sort()
+
+if N >= len(answer):
+    print(-1)
+else:
+    print(answer[N])
+
+
