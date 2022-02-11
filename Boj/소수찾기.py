5dd9bdca88fad3b99cbfe17784a8a244ab6876f3
Junyoung <wnsdud12365@naver.com>
2022-02-11 18:24:44 +0900

    20220211

---
+++ Boj/소수찾기.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n = input()
+nums = list(map(int, input().split()))
+
+count = 0
+for i in nums:
+    if i == 1:
+        continue
+    
+    for j in range(2, int(i**0.5)):
+        if i%j == 0:
+            break
+    else:
+        count += 1
+
+print(count)
