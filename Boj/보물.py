c32f88c821a76f06f98fbe7c4e1c6ba903835022
Junyoung <wnsdud12365@naver.com>
2022-02-14 18:02:11 +0900

    20220214

---
+++ Boj/보물.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+a = sorted(a, reverse=True)
+b = sorted(b)
+
+sum = 0
+for i in range(n):
+    sum += a[i]*b[i]
+    
+print(sum)
