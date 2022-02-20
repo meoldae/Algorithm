f924be84d68a7f580f94d24fd0f991f0fd26be45
Junyoung <wnsdud12365@naver.com>
2022-02-20 20:29:52 +0900

    20220220

--- Programmers/여행경로.py
+++ Programmers/여행경로.py
@@ -10,12 +10,19 @@ def solution(tickets):
             route[ticket[0]] = [ticket[1]]
     
     for r in route:
-        route[r] = sorted(route[r])
+        route[r] = sorted(route[r], reverse=True)
     
-    queue = deque()
     
+    temp = ["ICN"]
+    
+    while temp:
+        dst = temp[-1]
+        if dst not in route or len(route[dst]) == 0:
+            answer.append(temp.pop())
+        else:
+            temp.append(route[dst].pop())
         
-    return answer
+    return answer[::-1]
 
 if __name__ == '__main__':
     # tickets = [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]
