c79dd652992cb11c1c7211bf6f42fe9b0ec59451
Junyoung <wnsdud12365@naver.com>
2022-02-22 17:24:22 +0900

    20220222

---
+++ Boj/요세푸스문제.py
@@ -0,0 +1,17 @@
+from collections import deque
+n, k = map(int, input().split())
+
+answer = []
+queue = deque([i for i in range(1, n+1)])
+count = 1
+
+while queue:
+    temp = queue.popleft()    
+    if count % k == 0:
+        answer.append(str(temp))
+    else:
+        queue.append(temp)
+    count += 1
+
+print("<%s>" %(", ".join(answer)))
+    
