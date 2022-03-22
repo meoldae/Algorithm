0005ed0ce7caf2d23c27a14355f54260c784d218
Junyoung <wnsdud12365@naver.com>
2022-03-22 11:53:31 +0900

    20220322

--- Boj/좌표정렬하기2.py
+++ Boj/좌표정렬하기2.py
@@ -7,7 +7,7 @@ for _ in range(n):
     x, y = map(int, input().split())
     points.append((x, y))
 
-points.sort(key = lambda x:x[1])
+points.sort(key = lambda x:(x[1], x[0]))
 
 for a, b in points:
     print(a, b)
