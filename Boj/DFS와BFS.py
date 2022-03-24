b274bdabbe2fb65015c00b6043df7c29dd58a32c
Junyoung <wnsdud12365@naver.com>
2022-03-24 21:43:44 +0900

    20220324

--- Boj/DFS와BFS.py
+++ Boj/DFS와BFS.py
@@ -3,28 +3,27 @@ from collections import deque
 input = sys.stdin.readline
 
 n, m, v = map(int, input().split())
-graph = [[0]*(n+1) for i in range(n+1)]
-visited_dfs = [-1] * (n+1)
-visited_bfs = [-1] * (n+1)
+graph = [[0]*(n+1) for _ in range(n+1)]
+visited_dfs = [0] * (n+1)
+visited_bfs = [0] * (n+1)
 
 def dfs(v):
     visited_dfs[v] = 1
     print(v, end=" ")
     for i in range(1, n+1):
-        if visited_dfs[i] == -1 and graph[v][i] == 1:
+        if visited_dfs[i] == 0 and graph[v][i] == 1:
             dfs(i)
     
 
 def bfs(v):
-    visited_bfs[v] = 1
     queue = deque()
     queue.append(v)
+    visited_bfs[v] = 1
     while queue:
-        print(queue)
         temp = queue.popleft()
         print(temp, end=" ")
         for i in range(1, n+1):
-            if visited_bfs[i] == -1 and graph[v][i] == 1:
+            if visited_bfs[i] == 0 and graph[temp][i] == 1:
                 queue.append(i)
                 visited_bfs[i] = 1
                 
