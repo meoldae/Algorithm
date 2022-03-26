4965b7574369bdc3ab5902f5d5d4c4d3e4b2bade
Junyoung <wnsdud12365@naver.com>
2022-03-26 21:20:19 +0900

    20220326

--- Boj/트리.py
+++ Boj/트리.py
@@ -10,29 +10,33 @@ rm = int(input())
 tree = defaultdict(list)
 
 for i in range(n):
-    if parents[i] != -1 and i != rm:
+    if parents[i] == -1 or i == rm: 
+        continue
+    else:
         tree[parents[i]].append(i)
 
 count = 0
 
-def dfs(root):
+def dfs(root, rm):
     global count
+    
+    if not tree[root]:
+        count += 1
+        return
+    
     temp = list(tree[root])
     for word in temp:
-        if word not in tree:
+        if word == rm:
             count += 1
         else:
-            dfs(word)
+            dfs(word, rm)
     return
-
-
-del parents[rm]
 
 for i in range(len(parents)):
     if parents[i] == -1:
-        if tree[i] == []:
-            count += 1 
+        if i == rm:
+            break
         else:
-            dfs(i)
+            dfs(i, rm)
                 
 print(count)
