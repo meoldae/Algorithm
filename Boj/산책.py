c29c90a8c2134cc95e8b04591b9012c433543c1f
Junyoung <wnsdud12365@naver.com>
2022-04-27 15:27:17 +0900

    20220427

--- Boj/산책.py
+++ Boj/산책.py
@@ -5,19 +5,48 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 graph = defaultdict(list)
-visited = []
+    
 for _ in range(m):
     a, b = map(int, input().split())
-    
+        
     graph[a].append(b)
     graph[b].append(a)
+
+for i in range(1, n+1):
+    graph[i].sort()
     
 s, e = map(int, input().split())
+visited = [0]*(n+1)
+global_load = []
 
-count = 0
-
-# Node, count, Visited
-queue = deque([s, 0, visited])
-
-while queue:
+def bfs(start, goal):        
+    global global_load
+    queue = deque()
+    queue.append([start, 0, []])
     
+    visited[start] = 1
+    
+    while queue:
+        node, count, load = queue.popleft()
+        count += 1
+
+        for i in graph[node]:
+            temp_load = load[::]
+            if i == goal:
+                global_load = temp_load[::]
+                return count                
+            else:
+                if visited[i] == 0:
+                    visited[i] = 1
+                    temp_load.append(i)
+                    queue.append([i, count, temp_load])
+    return
+
+val = bfs(s, e)
+
+visited = [0]*(n+1)
+for i in global_load:
+    visited[i] = 1
+
+val += bfs(e, s)
+print(val)
