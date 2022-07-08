051b11d0166471dbd60294972b2bc45eb67171f0
Junyoung <wnsdud12365@naver.com>
2022-07-08 21:42:07 +0900

    Algorythm

--- Boj/스타트링크.py
+++ Boj/스타트링크.py
@@ -3,17 +3,37 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-floor, src, dst, up, down = map(int, input().split())
+def solution():
+    floor, src, dst, up, down = map(int, input().split())
 
-queue = deque()
+    if src == dst:
+        print(0)
+        return
 
-queue.append(src)
+    queue = deque()
 
-while queue:
+    queue.append([src, 0])
+    visited = [1e9]*(floor+1)
+    visited[src] = 1
     
-    for _ in range(len(queue))
-        now = queue.popleft()
+    while queue:
+        now, count = queue.popleft()
         
-        
+        for d in [up, -down]:
+            next = now+d
+            if next <= 0 or next > floor:
+                continue
+            
+            if next == dst:
+                print(count+1)
+                return
+            # print(visited)
+            if visited[next] > count+1:
+                queue.append([next, count+1])
+                visited[next] = count+1
+    else:
+        print('use the stairs')
     
-    
+    return
+
+solution()
