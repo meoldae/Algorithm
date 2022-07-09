c3dab13452f6853bda95a3b9d6a7c2c460d14d00
Junyoung <wnsdud12365@naver.com>
2022-07-09 17:21:03 +0900

    Algorythm

--- Boj/합분해.py
+++ Boj/합분해.py
@@ -4,5 +4,17 @@ input = sys.stdin.readline
 
 n, k = map(int, input().split())
 
-dp = [[0]*n for i in range(k)]
-print(dp)
+dp = [[0]*(n) for _ in range(k)]
+
+for i in range(n):
+    dp[0][i] = 1
+    dp[1][i] = i+2
+
+for i in range(k):
+    dp[i][0] = i+1
+  
+for a in range(2, k):
+    for b in range(1, n):
+        dp[a][b] = dp[a-1][b] + dp[a][b-1]
+
+print(dp[k-1][n-1]%1000000000)
