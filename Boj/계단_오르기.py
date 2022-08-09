6555d9a8814bf27f625b2ad678da67823c72f5a3
Junyoung <wnsdud12365@naver.com>
2022-08-09 21:01:00 +0900

    Algorythm

--- Boj/계단_오르기.py
+++ Boj/계단_오르기.py
@@ -15,7 +15,8 @@ if n == 1:
 else:
     dp[0] = stairs[0]
     dp[1] = stairs[0] + stairs[1]
-    dp[2] = max(stairs[1]+stairs[2], stairs[0]+stairs[2]) # 2 3 or 1 3
+    if n > 2:
+        dp[2] = max(stairs[1]+stairs[2], stairs[0]+stairs[2]) # 2 3 or 1 3
     
     for i in range(3, n):
         dp[i] = max(dp[i-3]+stairs[i-1]+stairs[i], dp[i-2]+stairs[i]) # Last-1 O or Last-1 X
