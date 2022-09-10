405415daf19c92d1ba4170b9c2865c09d68ebb7b
Junyoung <wnsdud12365@naver.com>
2022-09-10 21:57:46 +0900

    Algorythm

---
+++ Boj/가장_긴_감소하는_부분_수열.py
@@ -0,0 +1,15 @@
+#Baekjoon 11722
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int, input().split()))
+
+answer = [1] * n
+
+for i in range(1, n):
+    for j in range(i):
+        if a[i] < a[j]:
+            answer[i] = max(answer[i], answer[j]+1)
+
+print(max(answer))
