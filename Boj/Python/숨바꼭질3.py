420da8d403eece4b808f59a21e37ef0f8c1615ad
Junyoung <wnsdud12365@naver.com>
2022-10-25 18:44:39 +0900

    Algorythm

--- Boj/Python/숨바꼭질3.py
+++ Boj/Python/숨바꼭질3.py
@@ -5,34 +5,27 @@ input = sys.stdin.readline
 
 n, k = map(int, input().split())
 
-def bfs(n, k):
-    queue = deque()
-    queue.append((n, 0))
-    visited = [0]*(400001)
-    while queue:
-        now, count = queue.popleft()
-            
-        visited[now] = count
+visited = [-1] * 100001
+visited[n] = 0
 
-        plus = now+1
-        mul = now*2
-        minus = now-1
-            
-        if k in [plus, minus]:
-            return count + 1
-        elif k == mul:
-            return count
-        
-        if visited[plus] == 0 or visited[plus] > count+1:
-            visited[plus] = count+1
-            queue.append((plus, count+1))
-        
-        if visited[mul] == 0 or visited[mul] > count:
-            visited[mul] = count
-            queue.append((mul, count))
-        
-        if (visited[minus] == 0 or visited[minus] > count+1) and minus >= 0:
-            visited[minus] = count+1
-            queue.append((minus, count+1))
+queue = deque()
+queue.append(n)
 
-print(bfs(n, k))
+while queue:
+    now = queue.popleft()
+    
+    if now == k:
+        print(visited[k])
+        break
+    
+    if now - 1 >= 0 and visited[now-1] == -1:
+        queue.append(now-1)
+        visited[now-1] = visited[now] + 1
+    
+    if 0 <= now*2 < 100001 and visited[now*2] == -1:
+        queue.appendleft(now*2)
+        visited[now*2] = visited[now]
+    
+    if 0 <= now + 1 < 100001 and visited[now+1] == -1:
+        queue.append(now+1)
+        visited[now+1] = visited[now] + 1
