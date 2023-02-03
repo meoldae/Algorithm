bf53beb32788f622a9f4adec85ed805ca479eb64
Junyoung <wnsdud12365@naver.com>
2023-02-03 11:01:26 +0900

    Name Edited

--- Boj/Java/src/B2749.java
+++ Boj/Java/src/B2749.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class B11444 {
+public class B2749 {
     static int[] fibo;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
