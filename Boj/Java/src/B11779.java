7268e73da3c8bdbed9b55158e97c3356b57d3121
Junyoung <wnsdud12365@naver.com>
2023-02-13 11:15:13 +0900

    Algorythm

--- Boj/Java/src/B11779.java
+++ Boj/Java/src/B11779.java
@@ -4,80 +4,70 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class B11779 {
-    static class Node {
-        int location;
-        int cost;
-
-        public Node(int location, int cost) {
-            this.location = location;
-            this.cost = cost;
-        }
-    }
-
-    static long[] distance;
-    static int[][] route;
-    static int N;
+    static ArrayList<ArrayList<int[]>> graph;
+    static int[] path;
+    static int[] distance;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
-        route = new int[N][N];
-        distance = new long[N];
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        path = new int[n];
+        graph = new ArrayList<>();
+        distance = new int[n];
         Arrays.fill(distance, Integer.MAX_VALUE);
 
-        for (int i = 0; i < M; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int src = Integer.parseInt(st.nextToken()) - 1;
-            int dst = Integer.parseInt(st.nextToken()) - 1;
-            int cost = Integer.parseInt(st.nextToken());
-            if (route[src][dst] == 0) route[src][dst] = cost;
-            else route[src][dst] = Math.min(route[src][dst], cost);
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
         }
-
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken()) - 1;
+            int end = Integer.parseInt(st.nextToken()) - 1;
+            int cost = Integer.parseInt(st.nextToken());
+            graph.get(start).add(new int[]{end, cost});
+        }
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int start = Integer.parseInt(st.nextToken()) - 1;
-        int end = Integer.parseInt(st.nextToken()) - 1;
-
-        dijkstra(start, end);
+        int s = Integer.parseInt(st.nextToken()) - 1;
+        int e = Integer.parseInt(st.nextToken()) - 1;
+        dijkstra(s, e);
     }
 
-    public static void dijkstra(int start, int end) {
-        int[] path = new int[N];
-        StringBuilder sb = new StringBuilder();
-        PriorityQueue<Node> heap = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
-        heap.add(new Node(start, 0));
+    static void dijkstra(int start, int end) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+        pq.add(new int[]{start, 0});
         distance[start] = 0;
 
-        while (!heap.isEmpty()) {
-            Node now = heap.poll();
-            int dist = now.cost;
-            int loca = now.location;
+        while (!pq.isEmpty()) {
+            int[] now = pq.poll();
+            int dist = now[1];
+            int location = now[0];
 
-            if (distance[loca] < dist) {
+            if (distance[location] < dist) {
                 continue;
             }
-
-            for (int i = 0; i < N; i++) {
-                if (route[loca][i] == 0) continue;
-                int nextCost = dist + route[loca][i];
-                if (nextCost <= distance[i]) {
-                    distance[i] = nextCost;
-                    path[i] = now.location;
-                    heap.add(new Node(i, nextCost));
+            for (int[] next : graph.get(location)) {
+                int nextCost = dist + next[1];
+                if (nextCost < distance[next[0]]) {
+                    distance[next[0]] = nextCost;
+                    path[next[0]] = location;
+                    pq.add(new int[]{next[0], nextCost});
                 }
             }
         }
         System.out.println(distance[end]);
-        int count = 1;
-        for (int i = end; i != 0; i = path[i]){
-            sb.append(i+1).append(" ");
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+        for (int i = end; i != start; i = path[i]) {
+            stack.push(i+1);
             count++;
         }
-        sb.append(start+1);
-        sb.reverse();
-        sb.insert(0, count+"\n");
+        System.out.println(count+1);
+        sb.append(start+1).append(" ");
+        while(!stack.isEmpty()){
+            sb.append(stack.pop()).append(" ");
+        }
         System.out.println(sb);
     }
 }
