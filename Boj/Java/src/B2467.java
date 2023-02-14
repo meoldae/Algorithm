2218adfb300c4249d3c9194295475b7fb31fd012
Junyoung <wnsdud12365@naver.com>
2023-02-14 10:48:29 +0900

    Algorythm

--- Boj/Java/src/B2467.java
+++ Boj/Java/src/B2467.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class B2467 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class B2467 {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dissolve = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int left = 0;
+        int right = n-1;
+        int value = Integer.MAX_VALUE;
+        int[] answer = new int[2];
+
+        while (left < right){
+            int sum = dissolve[left] + dissolve[right];
+            if (value > Math.abs(sum)){
+                value = Math.abs(sum);
+                answer[0] = left;
+                answer[1] = right;
+            }
+            if (sum > 0) {
+                right--;
+            }else {
+                left++;
+            }
+        }
+        System.out.println(dissolve[answer[0]] + " " + dissolve[answer[1]]);
+    }
 }
