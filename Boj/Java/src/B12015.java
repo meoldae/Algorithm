ae19573c8ae88fd33c1b2e241183fd0ec326369d
Junyoung <wnsdud12365@naver.com>
2023-02-25 16:33:27 +0900

    Algorithm

---
+++ Boj/Java/src/B12015.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class B12015 {
+    static int[] numbers, tempLis;
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        tempLis = new int[N];
+
+        int right = 0;
+        for (int i = 0; i < N; i++) {
+            if (tempLis[right] < numbers[i]) {
+                right++;
+                if (right == N) {
+                    System.out.print(right);
+                    return;
+                }
+                tempLis[right] = numbers[i];
+            } else {
+                int idx = binarySearch(numbers[i], 0, right);
+                tempLis[idx] = numbers[i];
+            }
+        }
+        System.out.print(right);
+    }
+
+    static int binarySearch(int target, int left, int right) {
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (tempLis[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return right;
+    }
+}
