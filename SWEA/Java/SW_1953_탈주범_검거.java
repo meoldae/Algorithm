d13d42f5c36c166f04c0b6f2653922e18287d122
Junyoung <wnsdud12365@naver.com>
2023-02-28 20:20:03 +0900

    Algorithm

--- SWEA/Java/SW_1953_탈주범_검거.java
+++ SWEA/Java/SW_1953_탈주범_검거.java
@@ -10,8 +10,6 @@ public class SW_1953_탈주범_검거 {
     static int[][] direction = {{}, {0, 1, 2, 3}, {0, 2}, {1, 3}, {0, 1}, {1, 2}, {2, 3}, {3, 0}};
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
-    static int[][] board;
-    static int[][] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,26 +22,28 @@ public class SW_1953_탈주범_검거 {
             int startX = Integer.parseInt(st.nextToken());
             int startY = Integer.parseInt(st.nextToken());
             int time = Integer.parseInt(st.nextToken());
-            board = new int[N][M];
-            visited = new int[N][M];
+            int[][] board = new int[N][M];
+            int[][] visited = new int[N][M];
             for (int i = 0; i < N; i++) {
                 board[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             }
             Queue<int[]> queue = new LinkedList<>();
             queue.offer(new int[]{startX, startY, 1});
-            visited[startX][startY] = 1;
-            int answer = 1;
+            int answer = 0;
             while (!queue.isEmpty()) {
                 int[] now = queue.poll();
-                if (now[2] == time) break;
+                if (now[2] > time) break;
+
+                if (visited[now[0]][now[1]] == 1) continue;
+                visited[now[0]][now[1]] = 1;
+                answer++;
+
                 for (int d : direction[board[now[0]][now[1]]]) {
                     int nx = now[0] + dx[d];
                     int ny = now[1] + dy[d];
 
                     if (0 <= nx && nx < N && 0 <= ny && ny < M) {
-                        if (visited[nx][ny] == 0 && isPossible(now[0], now[1], d)) {
-                            visited[nx][ny] = 1;
-                            answer++;
+                        if (isPossible(board, now[0], now[1], d)) {
                             queue.offer(new int[]{nx, ny, now[2] + 1});
                         }
                     }
@@ -54,7 +54,7 @@ public class SW_1953_탈주범_검거 {
         System.out.print(sb);
     }
 
-    static boolean isPossible(int x, int y, int d) {
+    static boolean isPossible(int[][] board, int x, int y, int d) {
         int nextType = board[x + dx[d]][y + dy[d]];
         if (nextType == 0) return false;
         for (int next : direction[nextType]) {
