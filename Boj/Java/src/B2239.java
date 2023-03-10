8141ac3c5af22522203aa76696f98b3d9687ae4e
Junyoung <wnsdud12365@naver.com>
2023-03-10 11:01:55 +0900

    Algorithm

--- Boj/Java/src/B2239.java
+++ Boj/Java/src/B2239.java
@@ -1,12 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class B2239 {
     static boolean[][] rowVisit = new boolean[9][9];
     static boolean[][] colVisit = new boolean[9][9];
     static boolean[][] districtVisit = new boolean[9][9];
-    static char[][] board = new char[9][];
+    static int[][] board = new int[9][9];
     static boolean flag;
 
     public static void main(String[] args) throws IOException {
@@ -17,45 +18,43 @@ public class B2239 {
                 if (row[j] != '0') {
                     rowVisit[i][(row[j] - '0') - 1] = true;
                     colVisit[j][(row[j] - '0') - 1] = true;
-                    districtVisit[(i / 3) + (j / 3)][(row[j] - '0') - 1] = true;
+                    districtVisit[((i / 3) * 3) + (j / 3)][(row[j] - '0') - 1] = true;
                 }
+                board[i][j] = row[j] - '0';
             }
-            board[i] = row;
         }
-        input(0, 0);
+        sudoku(0, 0);
     }
 
-    static void input(int x, int y) {
-        if (flag) return;
-        for (int j = y; j < 9; j++) {
-            for (int num = 0; num < 9; num++) {
-                if (board[x][j] == '0') {
-                    if (rowVisit[x][num] || colVisit[j][num] || districtVisit[(x / 3) + (j / 3)][num]) continue;
-                    board[x][j] = (char) num;
-                    input(x, j + 1);
-                    board[x][j] = '0';
-                }
+    static void sudoku(int x, int y) {
+        if(flag) return;
+        if (x == 8 && y == 9) {
+            for (int[] row : board) {
+                Arrays.stream(row).forEach(System.out::print);
+                System.out.println();
             }
+            flag = true;
+            return;
         }
 
-        for (int i = x + 1; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (i == 8 && j == 8) {
-                    flag = true;
-                    for (char[] c : board) {
-                        System.out.println(new String(c));
-                    }
-                    return;
+        if (y == 9) sudoku(x + 1, 0);
+
+        else {
+            if (board[x][y] == 0) {
+                for (int i = 0; i < 9; i++) {
+                    if (rowVisit[x][i] || colVisit[y][i] || districtVisit[((x / 3) * 3) + (y / 3)][i]) continue;
+                    rowVisit[x][i] = true;
+                    colVisit[y][i] = true;
+                    districtVisit[((x / 3) * 3) + (y / 3)][i] = true;
+                    board[x][y] = i + 1;
+                    sudoku(x, y + 1);
+                    rowVisit[x][i] = false;
+                    colVisit[y][i] = false;
+                    districtVisit[((x / 3) * 3) + (y / 3)][i] = false;
+                    board[x][y] = 0;
                 }
-                for (int num = 0; num < 9; num++) {
-                    if (board[i][j] == '0') {
-                        if (rowVisit[i][num] || colVisit[j][num] || districtVisit[(i / 3) + (j / 3)][num]) continue;
-                        board[i][j] = (char) num;
-                        input(x, j + 1);
-                        board[i][j] = '0';
-                    }
-                }
-            }
+            } else sudoku(x, y + 1);
+
         }
     }
 }
