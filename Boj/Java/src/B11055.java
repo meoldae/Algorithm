c26da72114188f4c718e5f6b75f565e06abc1552
Junyoung <wnsdud12365@naver.com>
2023-03-18 20:54:22 +0900

    Algorithm

---
+++ Boj/Java/src/B11055.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class B11055 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] dp = new int[N];
+        int maxVal = 0;
+
+        dp[0] = numbers[0];
+        for (int i = 1; i < N; i++) {
+            dp[i] = numbers[i];
+            for (int j = 0; j < i; j++) {
+                if (numbers[i] > numbers[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + numbers[i]);
+                }
+            }
+            maxVal = Math.max(maxVal, dp[i]);
+        }
+        System.out.print(maxVal);
+    }
+}
