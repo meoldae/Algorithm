0c510c57bf6e1a6f9d6838d1631f66d862aedb0b
Jeon <wnsdud12365@naver.com>
2023-05-10 09:12:21 +0900

    [Refactor] G5 : BFS

--- Boj/Java/src/B17836.java
+++ Boj/Java/src/B17836.java
@@ -24,11 +24,13 @@ public class B17836 {
             }
         }
 
-        int[][][] visited = new int[2][N][M];
+        int[][] visited = new int[N][M];
 
         Queue<int[]> queue = new ArrayDeque<>();
-        queue.offer(new int[]{0, 0, 0});
-        visited[0][0][0] = 1;
+        queue.offer(new int[]{0, 0});
+        visited[0][0] = 1;
+
+        int answer = 0;
 
         while (!queue.isEmpty()) {
             int[] now = queue.poll();
@@ -39,31 +41,21 @@ public class B17836 {
 
                 if (0 > nx || nx >= N || 0 > ny || ny >= M) continue;
 
-                if (now[2] == 0) {
-                    if (visited[now[2]][nx][ny] == 0 && castle[nx][ny] != 1) {
-                        if (castle[nx][ny] == 2) {
-                            visited[1][nx][ny] = visited[0][now[0]][now[1]] + 1;
-                            queue.offer(new int[]{nx, ny, 1});
-                        }
-                        visited[now[2]][nx][ny] = visited[now[2]][now[0]][now[1]] + 1;
-                        queue.offer(new int[]{nx, ny, now[2]});
+                if (visited[nx][ny] == 0 && castle[nx][ny] != 1) {
+                    if (castle[nx][ny] == 2) {
+                        answer = (N - nx) + (M - ny) + visited[now[0]][now[1]] - 2;
                     }
-                } else {
-                    if (visited[now[2]][nx][ny] == 0) {
-                        visited[now[2]][nx][ny] = visited[now[2]][now[0]][now[1]] + 1;
-                        queue.offer(new int[]{nx, ny, now[2]});
-                    }
+                    visited[nx][ny] = visited[now[0]][now[1]] + 1;
+                    queue.offer(new int[]{nx, ny});
                 }
             }
         }
-        int answer = Math.min(visited[0][N-1][M-1] - 1, visited[1][N-1][M-1] - 1);
-        if (visited[0][N-1][M-1] - 1 == -1 || visited[1][N-1][M-1] - 1 == -1) {
-            answer = Math.max(visited[0][N-1][M-1] - 1, visited[1][N-1][M-1] - 1);
+
+        if (answer == 0 || visited[N - 1][M - 1] == 0) {
+            answer = Math.max(answer, visited[N - 1][M - 1] - 1);
+        } else {
+            answer = Math.min(answer, visited[N - 1][M - 1] - 1);
         }
-        if (answer == -1) {
-            System.out.print("Fail");
-        }else {
-            System.out.print(answer <= T ? answer : "Fail");
-        }
+        System.out.print((answer <= T && answer > 0) ? answer : "Fail");
     }
 }
