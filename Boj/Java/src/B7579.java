b799dbd85191a22537cdf2f302eea92be3d85617
Junyoung <wnsdud12365@naver.com>
2023-12-14 16:13:50 +0900

    G3: DP

--- Boj/Java/src/B7579.java
+++ Boj/Java/src/B7579.java
@@ -21,18 +21,18 @@ public class B7579 {
             item[i][COST] = Integer.parseInt(st1.nextToken());
             max += item[i][COST];
         }
+        int[] dp = new int[max + 1];
+        for (int i = 0; i < N; i++) {
+            for (int j = max; j >= item[i][COST]; j--) {
+                dp[j] = Math.max(dp[j - item[i][COST]] + item[i][MEMORY], dp[j]);
+            }
+        }
 
-        int[][] dp = new int[N + 1][max+1];
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= max; j++) {
-                if (item[i-1][COST] <= j) dp[i][j] = Math.max(dp[i][j], dp[i-1][j - item[i-1][COST]] + item[i-1][MEMORY]);
-                dp[i][j] = Math.max(dp[i][j], dp[i-1][j]);
-                if (i == N && dp[i][j] >= M) {
-                    System.out.print(j);
-                    return;
-                }
+        for (int i = 0; i <= max; i++) {
+            if (dp[i] >= M) {
+                System.out.println(i);
+                return;
             }
         }
     }
 }
-
