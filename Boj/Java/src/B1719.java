e5ef0c73d224aa46d07c85700fcc0274717d8346
Junyoung <wnsdud12365@naver.com>
2024-02-29 16:30:26 +0900

    G3: Floyd-Warshall

--- Boj/Java/src/B1719.java
+++ Boj/Java/src/B1719.java
@@ -1,38 +1,60 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class B1719 {
+    static int[][] path;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int[][] graph = new int[N][N];
+        int[][] graph = new int[N + 1][N + 1];
+        path = new int[N + 1][N + 1];
+
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(graph[i + 1], Integer.MAX_VALUE);
+            graph[i + 1][i + 1] = 0;
+        }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int src = Integer.parseInt(st.nextToken()) - 1;
-            int dst = Integer.parseInt(st.nextToken()) - 1;
+            int src = Integer.parseInt(st.nextToken());
+            int dst = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
             graph[src][dst] = cost;
             graph[dst][src] = cost;
         }
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                for (int k = 0; k < N; k++) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                for (int k = 1; k <= N; k++) {
                     if (j == k) continue;
-                    if (graph[j][i] != 0 && graph[i][k] != 0) {
-                        graph[j][k] = Math.min(graph[j][k], graph[j][i] + graph[i][k]);
+                    if (graph[j][i] == Integer.MAX_VALUE || graph[i][k] == Integer.MAX_VALUE) continue;
+                    if (graph[j][k] > graph[j][i] + graph[i][k]) {
+                        graph[j][k] = graph[j][i] + graph[i][k];
+                        path[j][k] = i;
                     }
                 }
             }
         }
-        for (int[] row : graph) {
-            System.out.println(Arrays.toString(row));
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i == j) sb.append("-").append(" ");
+                else {
+                    sb.append(getPath(i, j)).append(" ");
+                }
+            }
+            sb.append("\n");
         }
+        System.out.print(sb);
+    }
+
+    public static int getPath(int start, int end) {
+        if (path[start][end] == 0) return end;
+        else return getPath(start, path[start][end]);
     }
 }
