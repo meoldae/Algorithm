15128956073de31a2cea2aa173620298b876a9b9
Junyoung <wnsdud12365@naver.com>
2025-04-01 23:36:13 +0900

    String.split() clause convert to StringTokenizer for performance

--- Boj/Java/src/B15724.java
+++ Boj/Java/src/B15724.java
@@ -1,19 +1,20 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class B15724 {    
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        int N = Integer.parseInt(input[0]);
-        int M = Integer.parseInt(input[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
         int[][] land = new int[N + 1][M + 1];
 
         for (int i = 1; i <= N; i++) {
-            input = br.readLine().split(" ");
+            st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= M; j++) {
-                land[i][j] = Integer.parseInt(input[j - 1]) + land[i - 1][j] + land[i][j - 1] - land[i - 1][j - 1];
+                land[i][j] = Integer.parseInt(st.nextToken()) + land[i - 1][j] + land[i][j - 1] - land[i - 1][j - 1];
             }
         }
 
@@ -21,11 +22,11 @@ public class B15724 {
 
         int K = Integer.parseInt(br.readLine());
         for (int i = 0; i < K; i++) {
-            input = br.readLine().split(" ");
-            int x1 = Integer.parseInt(input[0]);
-            int y1 = Integer.parseInt(input[1]);
-            int x2 = Integer.parseInt(input[2]);
-            int y2 = Integer.parseInt(input[3]);
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
 
             int answer = land[x2][y2] - land[x1 - 1][y2] - land[x2][y1 - 1] + land[x1 - 1][y1 - 1];
             sb.append(String.valueOf(answer)).append("\n");
